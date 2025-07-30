50313b963be483fb03666bb158f0c9046cad0006
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-07-30 19:24:39 -0400

    Created MovieCard component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,11 @@
-import './App.css'
+import './App.css';
+import MovieCard from './components/MovieCard';
 
 function App() {
   return (
     <>
-      
+      <MovieCard movie={{title: "Brian's Favorite", release_date: "2020"}} />
+      <MovieCard movie={{title: "Kyleigh's Favorite", release_date: "2001"}} />
     </>
   )
 }
