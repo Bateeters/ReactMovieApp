ed76cae6a865411a4861df3bbbd95e28b91a6fb8
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-07-30 19:30:17 -0400

    displaying conditional components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,10 +2,15 @@ import './App.css';
 import MovieCard from './components/MovieCard';
 
 function App() {
+  const movieNumber = 1;
+
   return (
     <>
-      <MovieCard movie={{title: "Brian's Favorite", release_date: "2020"}} />
-      <MovieCard movie={{title: "Kyleigh's Favorite", release_date: "2001"}} />
+      {movieNumber === 1 ? ( // If movieNumber equals 1
+        <MovieCard movie={{title: "Brian's Favorite", release_date: "2020"}} />
+       ) : ( // else
+        <MovieCard movie={{title: "Kyleigh's Favorite", release_date: "2001"}} />
+       )}
     </>
   )
 }
