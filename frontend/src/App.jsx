9e56584724ddc38610d7d51a9f5e7e093e782faa
Brian Teeters <BrianTeetersDesign@gmail.com>
2025-07-31 19:24:57 -0400

    set up state for search

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,13 @@
 import './App.css';
 import MovieCard from './components/MovieCard';
+import Home from './pages/Home';
 
 function App() {
   const movieNumber = 1;
 
   return (
     <>
-      {movieNumber === 1 ? ( // If movieNumber equals 1
-        <MovieCard movie={{title: "Brian's Favorite", release_date: "2020"}} />
-       ) : ( // else
-        <MovieCard movie={{title: "Kyleigh's Favorite", release_date: "2001"}} />
-       )}
+      <Home />
     </>
   )
 }
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,42 @@
+import MovieCard from "../components/MovieCard"
+import { useState } from "react"
+
+function Home() {
+    // creating a state for search bar
+    const [searchQuery, setSearchQuery] = useState(""); // setup is [variableName, setVariableName] = useState("defaultValue");
+
+    const movies =[
+        {id: 1, title: "John Wick", release_date: "2020"},
+        {id: 2, title: "John Wick 2", release_date: "2022"},
+        {id: 3, title: "John Wick 3", release_date: "2025"},
+    ]
+
+    const handleSearch = (e) => {
+        e.preventDefault() // does not refresh the page, keeping search bar text
+        alert(searchQuery) // alert now pops up with what was typed into search bar
+        setSearchQuery("RESET SEARCH!") // changes the search bar state to "Reset Search" on alert clear
+    }
+
+    return(
+        <div className="home">
+            <form onSubmit={handleSearch} className="search-form">
+                <input
+                  type="text"
+                  placeholder="Search for movies..." 
+                  className="search-input"
+                  value={searchQuery} // set the value of the created state
+                  onChange={(e) => setSearchQuery(e.target.value)} // get the current value, e, and using .target.value, it updates every keystroke
+                />
+                <button type="submit" className="search-button">Search</button>
+            </form>
+
+            <div className="movies-grid">
+                {movies.map((movie) => ( // using .map() to iterate through every movie in movies array
+                    <MovieCard movie={movie} key={movie.id}/> // for each movie, return the MovieCard component
+                ))}
+            </div>
+        </div>
+    )
+}
+
+export default Home
